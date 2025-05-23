a0d342b5631e2f4fbeadab51d7eeb79caac1c168
Aleksei Nikolaev <aleksei@mobilabsolutions.com>
2025-05-23 16:57:34 +0200

    added vars for clien_id an others

--- germanywestcentral/dev/variables.tf
+++ germanywestcentral/dev/variables.tf
@@ -1,3 +1,8 @@
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+
 variable "location" {
   description = "(Required) The location of the Resource Group."
 }
